766cd310c84c10f35cae1b189fd10bdb93e58ad9
Alexander Kuemmel <akisys@alexanderkuemmel.com>
2018-12-22 22:02:43 +0100

    FIX: handle already existing GID in entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,8 +7,10 @@ if [ -e /var/run/docker.sock ]; then
   if [ -z "${DGID}" ]; then
     (>&2 echo "Missing DGID env var"; exit 1)
   fi
+  set +e
   addgroup -g $DGID -S runtime
-  RUNGROUP="runtime"
+  set -e
+  RUNGROUP="$DGID"
 else
   RUNGROUP="gotty"
 fi
